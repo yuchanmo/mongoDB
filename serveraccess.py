bbdd3b8733e839844e4f944e31a0220170b7a4bf
yuchan.mo <mojjijji@gmail.com>
2019-03-14 17:00:23 +0900

    save

---
+++ serveraccess.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+import pymongo
+from pprint import pprint
+
+
+def osm():
+    client = MongoClient('ds1.snu.ac.kr', port=27018, username='dsteam2', password='dsteam2', authsource='dsteam2') 
+# db = client.dsteam2
+# db.list_collection_names()
+    db = client.osm 
+    return db
+
+pprint(db.list_collection_names())
+ss = list(db.map.find({'properties.admin':'South Korea'}))
+db.map.find({
+    'properties'
+})
+
+
+client.close()
